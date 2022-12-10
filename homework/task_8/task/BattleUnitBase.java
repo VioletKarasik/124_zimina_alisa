fecc7b15068ece3e1c457919fb6c320decf65033
VioletKarasik <51052509+VioletKarasik@users.noreply.github.com>
2022-12-10 23:24:45 +0300

    Update BattleUnitBase.java

--- homework/task_8/task/BattleUnitBase.java
+++ homework/task_8/task/BattleUnitBase.java
@@ -10,6 +10,7 @@ public abstract class BattleUnitBase implements BattleUnit {
         this.basicstrength = this.strength = basicstrength;
         this.maximamor = this.armor = maximarmor;
     }
+
     //Конструктор:
     protected final String name;
     protected int maximarmor;
@@ -21,5 +22,46 @@ public abstract class BattleUnitBase implements BattleUnit {
 
     //Абстрактные методы:
     public abstract void specialAbility(BattleUnit[] ownTeam, BattleUnit[] enemyTeam);
+
     public abstract void attack(BattleUnit other);
+
+
+    //реализованные методы
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public int health() {
+        return this.health;
+    }
+
+    @Override
+    public int maxHealth() {
+        return this.maximhealth;
+    }
+
+    @Override
+    public void setMaxHealth(int value) {
+        this.maximhealth = value;
+        if (this.health >= maximhealth) {
+            this.health = maximhealth;
+        }
+    }
+
+    @Override
+    public void heal(int value) {
+        this.health = this.health + value;
+        if (this.health >= maximhealth) {
+            this.health = maximhealth;
+        }
+    }
+
+    @Override
+    public void takeDamage(int value) {
+        this.health = this.health - value;
+        if (this.health <= 0) {
+            this.health = 0;
+        }
+    }
 }
