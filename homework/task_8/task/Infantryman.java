a570f040147b9b4d071b026e3104ef486471ab70
VioletKarasik <51052509+VioletKarasik@users.noreply.github.com>
2022-12-10 23:24:57 +0300

    Update Infantryman.java

--- homework/task_8/task/Infantryman.java
+++ homework/task_8/task/Infantryman.java
@@ -1,58 +1,12 @@
 package task;
 
 public class Infantryman extends BattleUnitBase {
-    //Конструктор:
-    public Infantryman (String name, int maximhealth, int basicstrength, int maximarmor) {
-        super(name, maximhealth, basicstrength, maximarmor);
-    }
-    //Методы:
-    @Override
-    public void specialAbility (BattleUnit[] ownTeam, BattleUnit[] enemyTeam) {
-    }
-    @Override
-    public void attack (BattleUnit other) {
-        double Strength1 = (this.strength()/2), Strength2 = (this.strength()/4);
-        if (Strength1 < 1){
-            Strength1 = 1;
-        }
-        if (Strength2 < 1){
-            Strength2 = 1;
-        }
-        if (other.armor() > 0){
-            other.takeDamage((int) Strength1);
-            other.damageArmor((int) Strength2);
-        }
-        else {
-            other.takeDamage((int) this.strength);
-        }
-    }
-    @Override
-    public String name() {
-        return null;
+    public Infantryman (String name, int maxHealth, int baseStrength, int maxArmor) {
+        super(name, maxHealth, baseStrength, maxArmor);
     }
 
     @Override
-    public int health() {
-        return 0;
-    }
-
-    @Override
-    public int maxHealth() {
-        return 0;
-    }
-
-    @Override
-    public void setMaxHealth(int value) {
-
-    }
-
-    @Override
-    public void heal(int value) {
-
-    }
-
-    @Override
-    public void takeDamage(int value) {
+    public void specialAbility(BattleUnit[] ownTeam, BattleUnit[] enemyTeam) {
 
     }
 
@@ -93,6 +47,11 @@ public class Infantryman extends BattleUnitBase {
 
     @Override
     public void setMaxArmor(int value) {
+
+    }
+
+    @Override
+    public void attack (BattleUnit other) {
 
     }
 }
