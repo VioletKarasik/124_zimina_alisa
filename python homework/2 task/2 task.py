76f401998ea7c6ae1f0d1f44da5eace99c204da3
VioletKarasik <51052509+VioletKarasik@users.noreply.github.com>
2023-03-10 22:28:03 +0300

    2 task

--- python homework/2 task/2 task.py
+++ python homework/2 task/2 task.py
@@ -1,25 +1,43 @@
 import requests
 import urllib.parse
 
+def parseEvents(text, count):
+    events = []
+
+    index = text.find('id="mw-pages"') 
+    while count > 0 and index != -1:
+        a_start = text.find("<li>", index) + 4
+        a_end = text.find(">", a_start) + 1
+        a_close = text.find("<", a_end)
+        events.append(text[a_end:a_close])
+        index = a_close
+        count -= 1
+    return events  
+
 def season_events(number_of_month, year):
 
-    months = ["Январь", "Февраль", "Март"]    
-    url = "Категория:{}_{}_Года".format(months[number_of_month - 1], year)
-    url = urllib.parse.quote(url)
-    url = "https://ru.wikipedia.org/wiki/" + url
+    months = ["Январь", "Февраль", "Март", 
+              "Апрель", "Май", "Июнь", 
+              "Июль", "Август", "Сентябрь", 
+              "Октябрь", "Ноябрь", "Январь"]    
+    
+    # Формирования url
+    url = "Категория:{}_{}_года".format(months[number_of_month - 1], year)
+    url = "https://ru.wikipedia.org/wiki/" + urllib.parse.quote(url) 
+
+    # Получение страницы
     r = requests.get(url)
     body = r.text
 
-    titleIndex = body.find("h2")
-    body = body[titleIndex:]
-    print(body)
+    # Сбор событий
+    events = parseEvents(body, 5)
+    
+    # Запись в файл
+    file = open("wiki.txt", "w")
+    text = "Вы родились в {} в {} году: \n{}".format(months[number_of_month - 1], year, "\n".join(events))
+    file.write(text)
+    file.close()
 
-
-
-    # file = open("wiki.txt", "w")
-    # source = "События"
-    # result = "«Вы родились в {} в {} году. {}».".format(months[number_of_month-1], year, source)
-    # file.write(result)
-    # file.close()
-
-season_events(2, 2000)
+month = int(input())
+year = int(input())
+season_events(month, year)
