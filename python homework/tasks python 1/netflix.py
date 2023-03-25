568b50064b7811ea810ca51b3b717bc9970f6d9a
VioletKarasik <51052509+VioletKarasik@users.noreply.github.com>
2023-03-26 01:07:54 +0300

    Create netflix.py

---
+++ python homework/tasks python 1/netflix.py
@@ -0,0 +1,14 @@
+def cost_n(x,y,z,cost):
+    if (abs((x+y)-cost) < abs((x+z)-cost)) and (abs((x+y)-cost) < abs((z+y)-cost)):
+        return "Alice and Bob can afford the subscription to Netflix"
+    elif (abs((x+z)-cost) < abs((y+z)-cost)) and (abs((x+z)-cost) < abs((x+y)-cost)):
+        return "Alice and Charlie can afford the subscription to Netflix"
+    elif (abs((z+y)-cost) < abs((x+y)-cost)) and (abs((z+y)-cost) < abs((z+x)-cost)):
+        return "Charlie and Bob can afford the subscription to Netflix"
+
+x = int(input("Enter Alice's amount of money: "))
+y = int(input("Enter Bob's amount of money: "))
+z = int(input("Enter Charlie's amount of money: "))
+cost = int(input("Enter the cost of Netflix subscription: "))
+
+print(cost_n(x,y,z,cost))
